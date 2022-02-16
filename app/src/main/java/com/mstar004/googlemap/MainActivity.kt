715718a72f8979b9d0779cb0bb00c877bdc71431
MstarDev <maqsadjonotaboyev0@gmail.com>
2022-02-16 17:40:28 +0500

    QR maker, Timer, CountDown vie changing color to danger

--- app/src/main/java/com/mstar004/googlemap/MainActivity.kt
+++ app/src/main/java/com/mstar004/googlemap/MainActivity.kt
@@ -2,44 +2,53 @@ package com.mstar004.googlemap
 
 
 import android.annotation.SuppressLint
+import android.app.*
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
+import android.media.AudioAttributes
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Vibrator
+import android.provider.Settings
 import android.view.View
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.mstar004.googlemap.databinding.ActivityMapsBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMapsBinding
+    private val CHANNEL_ID = "simple_notification"
+    private val NOTIFICATION_ID = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.btnMake.setOnClickListener {
 
-            object : CountDownTimer(15000, 1000){
+            object : CountDownTimer(15000, 1000) {
                 @SuppressLint("ResourceAsColor", "SetTextI18n")
                 override fun onTick(p0: Long) {
-
                     binding.orderTimer.text =
                         (p0 / 1000).toString().replace(".", "")
-                    when((p0/1000).toInt()){
+                    when ((p0 / 1000).toInt()) {
 
-                        10->{
+                        10 -> {
                             binding.orderTimer.setTextColor(Color.parseColor("#EECE21"))
                         }
-                        5-> {
+                        5 -> {
                             binding.orderTimer.setTextColor(Color.parseColor("#FA0606"))
                             binding.btnMake.setBackgroundResource(R.drawable.shape_red)
                         }
@@ -47,9 +56,10 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 override fun onFinish() {
-                    binding.btnMake.isEnabled = false
                     binding.orderTimer.visibility = View.GONE
                     binding.btnMake.setBackgroundResource(R.drawable.shape1)
+
+                    notification()
                 }
 
             }.start()
@@ -57,7 +67,7 @@ class MainActivity : AppCompatActivity() {
             val vibrator: Vibrator =
                 this.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
             if (vibrator.hasVibrator()) {
-                vibrator.vibrate(200)
+                vibrator.vibrate(100)
             }
 
             val json = binding.editText.text.toString().trim()
@@ -71,4 +81,45 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun notification() {
+
+        val resultIntent = Intent(this, MainActivity2::class.java)
+        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+
+        val builder = NotificationCompat.Builder(this@MainActivity, CHANNEL_ID)
+        builder.setAutoCancel(true)
+            .setSmallIcon(R.drawable.ic_baseline_timer_24)
+            .setContentTitle("QR maker")
+            .setContentText("QR image is done")
+            .setCategory(NotificationCompat.EXTRA_TEXT)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+            .setContentIntent(resultPendingIntent)
+            .setVibrate(longArrayOf(0, 1000, 1000, 1000))
+
+
+        val notifyManagerCompat = NotificationManagerCompat.from(this@MainActivity)
+        notifyManagerCompat.notify(NOTIFICATION_ID, builder.build())
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val name: CharSequence = "Simple"
+            val description = "Include all the simple notification"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val notificationChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            notificationChannel.description = description
+            val notificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+
+
+    }
+
+
 }
---
+++ app/src/main/java/com/mstar004/googlemap/MainActivity2.kt
@@ -0,0 +1,12 @@
+package com.mstar004.googlemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+    }
+}
